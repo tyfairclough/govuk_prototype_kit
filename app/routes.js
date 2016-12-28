a5a6b3b79aeab7e71688ebab7077cbc6390a9078
Paul Smith <mail@airfront.co.uk>
2016-12-28 20:09:51 +0000

    renames subapp routes file to make it less confusing

--- app/routes.js
+++ app/routes.js
@@ -4,7 +4,7 @@ var path = require('path')
 var config = require(path.join(__dirname + '/config.js'))
 
 // include sub-application routing if enabled in the configuration file
-if (config.useSubapplications) router.use('/', require(path.join(__dirname + '/subapp_routes.js')))
+if (config.useSubapplications) router.use('/', require(path.join(__dirname + '/subapps.js')))
 
 // Route index page
 router.get('/', function (req, res) {
