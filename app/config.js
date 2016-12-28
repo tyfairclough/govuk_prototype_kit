0436d0e5865beee81fec0df4df06433020e88f4a
Paul Smith <mail@airfront.co.uk>
2016-12-28 15:52:20 +0000

    adds subapplications flag to config

--- app/config.js
+++ app/config.js
@@ -19,6 +19,8 @@ module.exports = {
   useHttps: 'true',
 
   // Cookie warning - update link to service's cookie page.
-  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>'
+  cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#" title="Find out more about cookies">Find out more about cookies</a>',
+  
+  useSubapplications: true
 
 }
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,10 @@
 var express = require('express')
 var router = express.Router()
+var path = require('path')
+var config = require(path.join(__dirname + '/config.js'))
 
-// include sub-application routing
-router.use('/', require(__dirname + '/subapp_routes.js'))
+// include sub-application routing if enabled in the configuration file
+if (config.useSubapplications) router.use('/', require(path.join(__dirname + '/subapp_routes.js')))
 
 // Route index page
 router.get('/', function (req, res) {
