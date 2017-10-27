c63652aa0dd0e071486ad5ff212ed5395c70cfea
tyfairclough <ty@tyfairclough.com>
2017-10-27 11:50:38 +0100

    tweaks to index page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'ESFA digital apprenticeship service',
 
   // Default port that prototype runs on
   port: '3000',
