ce2ca91f350ba3cb2fc9278fce125c25af65d803
Ty Fairclough <ty.fairclough@gmail.com>
2017-12-13 17:47:06 +0000

    removed data sets

---
+++ app/views/apps/academy-v2/includes/scratchpad.js
@@ -0,0 +1,30 @@
+{
+    "users": [{
+        "id": 0,
+        "email": "ty.fairclough@gmail.com",
+        "topScore": 0,
+        "onboarding": [0, 0],
+        "levels": [{
+            "level1": [0, 0, 0],
+            "level2": [0, 0, 0]
+        }]
+    }, {
+        "id": 1,
+        "email": "tom.fairclough@gmail.com",
+        "topScore": 72,
+        "onboarding": [1, 1],
+        "levels": [{
+            "level1": [1, 1, 1],
+            "level2": [1, 1, 1]
+        }]
+    }, {
+        "id": 3,
+        "email": "tom",
+        "topScore": 0,
+        "onboarding": ["regular", "assessment"],
+        "levels": [{
+            "level1": [0, 1, 0, 1],
+            "level2": [0, 0, 0]
+        }]
+    }]
+}
